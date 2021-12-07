be545dedf57233f41527042885115591ec77fc73
mayssaharfouch <mayssaharfouch1@gmail.com>
2021-12-07 16:54:33 +0100

    Changes for fixing no visible binding for global variable note for gg_plotsim

--- R/gg_plotsim.R
+++ R/gg_plotsim.R
@@ -13,11 +13,11 @@
 #' @importFrom ggplot2 aes scale_x_continuous scale_y_continuous geom_line ggtitle geom_text geom_point sec_axis geom_col labs element_line element_blank element_text theme theme_bw
 #' @importFrom hrbrthemes theme_ipsum
 #' @importFrom data.table last
-#'
+#' @importFrom rlang .data
 
 # Update this function call
 utils::globalVariables(c("STEPS", "FertNH4_DOY", "hcut", "FertNO3_DOY", "FertNH4", "Irrig_DOY",
-                         "cut_DOY", "Irrig", "FertNO3" ))
+                         "cut_DOY", "Irrig", "FertNO3", "DOY"))
 
 gg_plotsim <- function(varsim, simmoy,onglet, opt_cut = NULL, opt_Irrig = NULL, opt_Fert = NULL) {
   var_ <- varsim
@@ -33,11 +33,10 @@ gg_plotsim <- function(varsim, simmoy,onglet, opt_cut = NULL, opt_Irrig = NULL, 
     coeff = 0.01
   }
   ## plot without cuts
-  plot_var <- ggplot2::ggplot(NULL, aes(x,y)) +
+  plot_var <- ggplot2::ggplot(NULL, aes(.data$x, .data$y)) +
     geom_line(data = simmoy,aes(x = STEPS, y = simmoy[,var_]), color="blue", size = 0.7)+
     scale_x_continuous(breaks = seq(0, last(simmoy$STEPS), by = 100)) +
     theme_ipsum() +
-    geom_text(x=3*min(simmoy$STEPS), y=0.98*max, size=4, label=name)+
     labs(x = "DOY", y = var_)+
     ggtitle(onglet)+
     theme_bw()+
@@ -55,28 +54,28 @@ gg_plotsim <- function(varsim, simmoy,onglet, opt_cut = NULL, opt_Irrig = NULL, 
     bln10 <- diff(break_init) < 10
     breaks_x <- replace(break_init, (c(FALSE, bln10) | c(bln10, FALSE)) & break_init %% 100 == 0, NA)
     plot_var +
-      geom_point(data = cut_DOY, aes(x = x, y = hcut/coeff), shape = 4, color = "black", size = 2)+
+      geom_point(data = cut_DOY, aes(x = DOY, y = hcut/coeff), shape = 4, color = "black", size = 2)+
       scale_x_continuous(breaks = breaks_x) +
       scale_y_continuous(
         # Add a second axis and specify its features
-        sec.axis = sec_axis(~.*coeff, name="hcut (m)"))
+        sec.axis = sec_axis(~.*coeff, name = "hcut (m)"))
   }
   # Irrigation
   else if  (!is.null(opt_Irrig)) {
     plot_var +
-      geom_col(data = Irrig_DOY, aes(x = x, y = Irrig/coeff), fill = "#8C00E1", alpha = 0.6)+
+      geom_col(data = Irrig_DOY, aes(x = DOY, y = Irrig/coeff), fill = "#8C00E1", alpha = 0.6)+
       scale_y_continuous(
         # Add a second axis and specify its features
-        sec.axis = sec_axis(~.*coeff, name="Irrigation"))
+        sec.axis = sec_axis(~.*coeff, name = "Irrigation"))
   }
   # Fertilization
   else if  (!is.null(opt_Fert)) {
     plot_var +
-      geom_col(data = FertNO3_DOY, aes(x = x, y = FertNO3/coeff), fill = "#B969B5", alpha = 0.6)+
-      geom_col(data = FertNH4_DOY, aes(x = x, y = FertNH4/coeff), fill = "#E69F00", alpha = 0.6)+
+      geom_col(data = FertNO3_DOY, aes(x = DOY, y = FertNO3/coeff), fill = "#B969B5", alpha = 0.6)+
+      geom_col(data = FertNH4_DOY, aes(x = DOY, y = FertNH4/coeff), fill = "#E69F00", alpha = 0.6)+
       scale_y_continuous(
         # Add a second axis and specify its features
-        sec.axis = sec_axis(~.*coeff, name="Fertilization"))
+        sec.axis = sec_axis(~.*coeff, name = "Fertilization"))
   }
   else {
     plot_var
