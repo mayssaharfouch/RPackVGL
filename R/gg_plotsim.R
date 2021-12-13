dc19dfa76ceaa6fb308c18f0dcfff2457e336b52
mayssaharfouch <mayssaharfouch1@gmail.com>
2021-12-13 17:24:36 +0100

    Adding coeff to gg_plotsim arguments

--- R/gg_plotsim.R
+++ R/gg_plotsim.R
@@ -19,21 +19,14 @@
 utils::globalVariables(c("STEPS", "FertNH4_DOY", "hcut", "FertNO3_DOY", "FertNH4", "Irrig_DOY",
                          "cut_DOY", "Irrig", "FertNO3", "DOY"))
 
-gg_plotsim <- function(varsim, simmoy,onglet, opt_cut = NULL, opt_Irrig = NULL, opt_Fert = NULL) {
+
+
+gg_plotsim <- function(varsim, simmoy,onglet,coeff = 1, opt_cut = NULL, opt_Irrig = NULL, opt_Fert = NULL) {
   var_ <- varsim
   min <- 0
   max <- 1.5*max(simmoy[,var_])
-  digits <- floor(log10(max)) + 1 #determine the nb of digits of the max value of 1st y-axis
-  # loop to determine the coeff used for the second axis scale
-  if (digits == 2 && is.nan(digits) == FALSE){
-    coeff = 1
-  } else if (digits == 3 && is.nan(digits) == FALSE){
-    coeff = 0.1
-  } else {
-    coeff = 0.01
-  }
   ## plot without cuts
-  plot_var <- ggplot2::ggplot(NULL, aes(.data$x, .data$y)) +
+  plot_var <- ggplot2::ggplot(NULL, aes(.data$x,.data$y)) +
     geom_line(data = simmoy,aes(x = STEPS, y = simmoy[,var_]), color="blue", size = 0.7)+
     scale_x_continuous(breaks = seq(0, last(simmoy$STEPS), by = 100)) +
     theme_ipsum() +
@@ -50,7 +43,7 @@ gg_plotsim <- function(varsim, simmoy,onglet, opt_cut = NULL, opt_Irrig = NULL, 
   # Cuts
   if (!is.null(opt_cut)) {
     # eliminate overlayed x-axis labels
-    break_init <- sort(c(seq(0, last(simmoy$STEPS), by = 100), cut_DOY$x), decreasing = FALSE)
+    break_init <- sort(c(seq(0, last(simmoy$STEPS), by = 100), cut_DOY$DOY), decreasing = FALSE)
     bln10 <- diff(break_init) < 10
     breaks_x <- replace(break_init, (c(FALSE, bln10) | c(bln10, FALSE)) & break_init %% 100 == 0, NA)
     plot_var +
@@ -58,24 +51,54 @@ gg_plotsim <- function(varsim, simmoy,onglet, opt_cut = NULL, opt_Irrig = NULL, 
       scale_x_continuous(breaks = breaks_x) +
       scale_y_continuous(
         # Add a second axis and specify its features
-        sec.axis = sec_axis(~.*coeff, name = "hcut (m)"))
+        sec.axis = sec_axis(~.*coeff, name= "hcut (cm)"))
   }
   # Irrigation
   else if  (!is.null(opt_Irrig)) {
-    plot_var +
+    plot_var <- ggplot2::ggplot(NULL, aes(.data$x,.data$y)) +
       geom_col(data = Irrig_DOY, aes(x = DOY, y = Irrig/coeff), fill = "#8C00E1", alpha = 0.6)+
+      geom_line(data = simmoy,aes(x = STEPS, y = simmoy[,var_]), color="blue", size = 0.7)+
+      scale_x_continuous(breaks = seq(0, last(simmoy$STEPS), by = 100)) +
       scale_y_continuous(
         # Add a second axis and specify its features
-        sec.axis = sec_axis(~.*coeff, name = "Irrigation"))
+        sec.axis = sec_axis(~.*coeff, name="Irrigation"))+
+      theme_ipsum() +
+      labs(x = "DOY", y = var_)+
+      ggtitle(onglet)+
+      theme_bw()+
+      theme(legend.text=element_text(family = "serif", size=10),
+            axis.text.x = element_text(angle = 90, size = 7),
+            axis.text.y = element_text(size = 7),
+            axis.title.x =element_text(family = "serif", size=10),
+            axis.title.y=element_text(family = "serif", size=10),
+            axis.line = element_line(colour = "black"), panel.background = element_blank(),
+            panel.grid.major = element_blank())
+
   }
   # Fertilization
   else if  (!is.null(opt_Fert)) {
-    plot_var +
+    break_init <- sort(c(seq(0, last(simmoy$STEPS), by = 100), FertNO3_DOY$DOY), decreasing = FALSE)
+    bln10 <- diff(break_init) < 10
+    breaks_x <- replace(break_init, (c(FALSE, bln10) | c(bln10, FALSE)) & break_init %% 100 == 0, NA)
+    plot_var <- ggplot2::ggplot(NULL, aes(.data$x,.data$y)) +
       geom_col(data = FertNO3_DOY, aes(x = DOY, y = FertNO3/coeff), fill = "#B969B5", alpha = 0.6)+
       geom_col(data = FertNH4_DOY, aes(x = DOY, y = FertNH4/coeff), fill = "#E69F00", alpha = 0.6)+
+      geom_line(data = simmoy,aes(x = STEPS, y = simmoy[,var_]), color="blue", size = 0.7)+
+      scale_x_continuous(breaks = breaks_x) +
       scale_y_continuous(
         # Add a second axis and specify its features
-        sec.axis = sec_axis(~.*coeff, name = "Fertilization"))
+        sec.axis = sec_axis(~.*coeff, name="Fertilization"))+
+      theme_ipsum() +
+      labs(x = "DOY", y = var_)+
+      ggtitle(onglet)+
+      theme_bw()+
+      theme(legend.text=element_text(family = "serif", size=10),
+            axis.text.x = element_text(angle = 90, size = 7),
+            axis.text.y = element_text(size = 7),
+            axis.title.x =element_text(family = "serif", size=10),
+            axis.title.y=element_text(family = "serif", size=10),
+            axis.line = element_line(colour = "black"), panel.background = element_blank(),
+            panel.grid.major = element_blank())
   }
   else {
     plot_var
