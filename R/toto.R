c20098e20755ccc61229289b25cf0d2a1adb47d5
mayssaharfouch <mayssaharfouch1@gmail.com>
2022-01-14 22:12:47 +0100

    Adding some modified files to fix a merge conflict

--- R/toto.R
+++ R/toto.R
@@ -8,11 +8,11 @@
 #'   \item{cutNB} {no unit}
 #'   \item{SurfPlante} {Total plant leaf area, in \ifelse{html}{\out{m<sup>2</sup>}}{\eqn{m^2}}}
 #'   \item{PARiPlante} {Plant intercepted PAR, in J.PAR}
-#'   \item{epsi} {no unit}
+#'   \item{epsi} {Light interception efficiency}
 #'   \item{dMSaer} {Dry matter production}
 #'   \item{Hplante} {Canopy height, in cm}
-#'   \item{Dplante} {no unit}
-#'   \item{RLTot} {no unit}
+#'   \item{Dplante} {Whole plant diameter in cm}
+#'   \item{RLTot} {Root length }
 #'   \item{RDepth} {Root depth, in m}
 #'   \item{MSaerien} {Total plant shoot dry biomass, in g}
 #'   \item{MSfeuil} {no unit}
@@ -20,49 +20,49 @@
 #'   \item{countSh} {no unit}
 #'   \item{countShExp} {no unit}
 #'   \item{demandC} {no unit}
-#'   \item{Leaf_Stem} {no unit}
+#'   \item{Leaf_Stem} {Rapport entre la biomasse de la repousse en cours avec les tiges}
 #'   \item{NBsh} {Number of primary shoots per plant}
 #'   \item{NBI} {Number of phytomers per primary axes of plant}
-#'   \item{NBD1} {no unit}
-#'   \item{NBB} {no unit}
+#'   \item{NBD1} {Nombre de bourgeons dormant de type 1}
+#'   \item{NBB} {Nombre de bourgeons B}
 #'   \item{FTSW} {Fraction of transpirable soil water}
-#'   \item{Etransp} {no unit}
+#'   \item{Etransp} {Daily plant transpiration}
 #'   \item{DemandC_Feuil} {The demand of leaves for carbon}
 #'   \item{DemandN_Pet} {The demand of petioles for nitrogen}
 #'   \item{DemandN_Stem} {The demand of stems for nitrogen}
 #'   \item{DemandN_Tot} {The demand of individual plants for nitrogen}
 #'   \item{DemandN_Tot_Aer} {The demand of aerial parts of individual plants for nitrogen}
-#'   \item{Npc_aer} {no unit}
+#'   \item{Npc_aer} {teneur en azote de la partie repousse (aerienne)}
 #'   \item{NNI} {Plant nitrogen nutrition index}
 #'   \item{Ndfa} {Proportion of N derived from fixation}
-#'   \item{Qfix} {no unit}
-#'   \item{Naerien} {no unit}
+#'   \item{Qfix} {qtt d'N issu de la fixation en gN}
+#'   \item{Naerien} {qtt d'azote dans les parties aérienne}
 #'   \item{Nuptake_sol} {no unit}
-#'   \item{R_DemandC_Root} {no unit}
-#'   \item{SRL} {no unit}
-#'   \item{dMSenFeuil} {no unit}
+#'   \item{R_DemandC_Root} {ratio entre l'offre et la demande de C pour les racines}
+#'   \item{SRL} {Specific root length}
+#'   \item{dMSenFeuil} {Daily flux de matiere seche senescente issu des parties feuilles en g/plante}
 #'   \item{dMSenTige} {no unit}
-#'   \item{MS_pivot} {no unit}
+#'   \item{MS_pivot} {matiere seche des pivots par plante}
 #'   \item{MS_rac_fine} {no unit}
 #'   \item{R_DemandC_Shoot} {no unit}
-#'   \item{RUEpot} {no unit}
+#'   \item{RUEpot} {RUE calcule en absence de stress}
 #'   \item{RUE} {Radiation use efficiency, in \ifelse{html}{\out{g.J.PAR<sup>-1</sup>}}{\eqn{g.J.PAR^-1}}}
 #'   \item{Npc_piv} {no unit}
 #'   \item{Npc_rac_fine} {no unit}
-#'   \item{dRLenSentot} {no unit}
+#'   \item{dRLenSentot} {variation de longeur de racines (perte)}
 #'   \item{dMSenRoot} {no unit}
-#'   \item{RLTotNet} {no unit}
-#'   \item{MS_rac_fineNet} {no unit}
-#'   \item{perteN_rac_fine} {no unit}
-#'   \item{NBphyto} {Number of phytomers}
-#'   \item{NBapexAct} {no unit}
+#'   \item{RLTotNet} {Longeur de racines vivantes sans la senescence}
+#'   \item{MS_rac_fineNet} {biomasse des racines fines vivantes}
+#'   \item{perteN_rac_fine} {flux d'azote lie a la senescence des racines fines}
+#'   \item{NBphyto} {total Number of phytomers}
+#'   \item{NBapexAct} {nombre d'apex actif}
 #'   \item{transpi} {no unit}
-#'   \item{cumtranspi} {no unit}
-#'   \item{aliveB} {no unit}
-#'   \item{TTphyllo} {no unit}
-#'   \item{dTT} {no unit}
-#'   \item{Udev} {no unit}
-#'   \item{Udevstress} {no unit}
+#'   \item{cumtranspi} {transpiration cumulee (debut de la levee)}
+#'   \item{aliveB} {status vivant lié a la presence de bourgeons actifs}
+#'   \item{TTphyllo} {Phyllocronic time}
+#'   \item{dTT} {variation journaliere de temps thermique}
+#'   \item{Udev} {Unité de developpement (voir stics)}
+#'   \item{Udevstress} {fct de temp photperide stress hydrique et stress azoté}
 #' }
 #'
 #' @docType data
