3aa61f780fbee760079ed1f33655fd35cb992270
mayssaharfouch <mayssaharfouch1@gmail.com>
2022-01-18 11:31:42 +0100

    Updating toto.R

--- R/toto.R
+++ R/toto.R
@@ -3,71 +3,71 @@
 #' An output example
 #'
 #' \itemize{
-#'   \item{TT} {Thermal time, in degree.days}
-#'   \item{time} {no unit}
-#'   \item{cutNB} {no unit}
-#'   \item{SurfPlante} {Total plant leaf area, in \ifelse{html}{\out{m<sup>2</sup>}}{\eqn{m^2}}}
-#'   \item{PARiPlante} {Plant intercepted PAR, in J.PAR}
-#'   \item{epsi} {Light interception efficiency}
-#'   \item{dMSaer} {Dry matter production}
-#'   \item{Hplante} {Canopy height, in cm}
-#'   \item{Dplante} {Whole plant diameter in cm}
-#'   \item{RLTot} {Root length }
-#'   \item{RDepth} {Root depth, in m}
-#'   \item{MSaerien} {Total plant shoot dry biomass, in g}
-#'   \item{MSfeuil} {no unit}
-#'   \item{MStot} {no unit}
-#'   \item{countSh} {no unit}
-#'   \item{countShExp} {no unit}
-#'   \item{demandC} {no unit}
-#'   \item{Leaf_Stem} {Rapport entre la biomasse de la repousse en cours avec les tiges}
-#'   \item{NBsh} {Number of primary shoots per plant}
-#'   \item{NBI} {Number of phytomers per primary axes of plant}
-#'   \item{NBD1} {Nombre de bourgeons dormant de type 1}
-#'   \item{NBB} {Nombre de bourgeons B}
-#'   \item{FTSW} {Fraction of transpirable soil water}
-#'   \item{Etransp} {Daily plant transpiration}
-#'   \item{DemandC_Feuil} {The demand of leaves for carbon}
-#'   \item{DemandN_Pet} {The demand of petioles for nitrogen}
-#'   \item{DemandN_Stem} {The demand of stems for nitrogen}
-#'   \item{DemandN_Tot} {The demand of individual plants for nitrogen}
-#'   \item{DemandN_Tot_Aer} {The demand of aerial parts of individual plants for nitrogen}
-#'   \item{Npc_aer} {teneur en azote de la partie repousse (aerienne)}
-#'   \item{NNI} {Plant nitrogen nutrition index}
-#'   \item{Ndfa} {Proportion of N derived from fixation}
-#'   \item{Qfix} {qtt d'N issu de la fixation en gN}
-#'   \item{Naerien} {qtt d'azote dans les parties aérienne}
-#'   \item{Nuptake_sol} {no unit}
-#'   \item{R_DemandC_Root} {ratio entre l'offre et la demande de C pour les racines}
-#'   \item{SRL} {Specific root length}
-#'   \item{dMSenFeuil} {Daily flux de matiere seche senescente issu des parties feuilles en g/plante}
-#'   \item{dMSenTige} {no unit}
-#'   \item{MS_pivot} {matiere seche des pivots par plante}
-#'   \item{MS_rac_fine} {no unit}
-#'   \item{R_DemandC_Shoot} {no unit}
-#'   \item{RUEpot} {RUE calcule en absence de stress}
-#'   \item{RUE} {Radiation use efficiency, in \ifelse{html}{\out{g.J.PAR<sup>-1</sup>}}{\eqn{g.J.PAR^-1}}}
-#'   \item{Npc_piv} {no unit}
-#'   \item{Npc_rac_fine} {no unit}
-#'   \item{dRLenSentot} {variation de longeur de racines (perte)}
-#'   \item{dMSenRoot} {no unit}
-#'   \item{RLTotNet} {Longeur de racines vivantes sans la senescence}
-#'   \item{MS_rac_fineNet} {biomasse des racines fines vivantes}
-#'   \item{perteN_rac_fine} {flux d'azote lie a la senescence des racines fines}
-#'   \item{NBphyto} {total Number of phytomers}
-#'   \item{NBapexAct} {nombre d'apex actif}
-#'   \item{transpi} {no unit}
-#'   \item{cumtranspi} {transpiration cumulee (debut de la levee)}
-#'   \item{aliveB} {status vivant lié a la presence de bourgeons actifs}
-#'   \item{TTphyllo} {Phyllocronic time}
-#'   \item{dTT} {variation journaliere de temps thermique}
-#'   \item{Udev} {Unité de developpement (voir stics)}
-#'   \item{Udevstress} {fct de temp photperide stress hydrique et stress azoté}
+#'   \item{TT:} {Thermal Time, in degree.days}
+#'   \item{time} {Time since the Start of the Simulation, in seconds}
+#'   \item{cutNB:} {Number of Cuts}
+#'   \item{SurfPlante:} {Total Plant Leaf Area, in \ifelse{html}{\out{m<sup>2</sup>}}{\eqn{m^2}}}
+#'   \item{PARiPlante:} {Plant Intercepted PAR, in J.PAR}
+#'   \item{epsi:} {Light Use Efficiency, in \ifelse{html}{\out{g dry matter MJ<sup>-1</sup>}}{\eqn{g dry matter MJ^-1}}}
+#'   \item{dMSaer:} {Dry Matter Production, in \ifelse{html}{\out{g.t<sup>-1</sup>}}{\eqn{g.t^-1}}}
+#'   \item{Hplante:} {Canopy Height, in m}
+#'   \item{Dplante:} {Plant Diameter, in m}
+#'   \item{RDepth:} {Root Depth, in m}
+#'   \item{MSaerien:} {Total Plant Shoot Dry Biomass, in g}
+#'   \item{MSfeuil:} {Leaves Dry Biomass, in g}
+#'   \item{RLTot:} {Root Length, in m}
+#'   \item{MStot:} {Total Plant Dry Biomass, in g}
+#'   \item{countSh} {}
+#'   \item{countShExp} {}
+#'   \item{demandC:} {Demand for Carbon, in g}
+#'   \item{Leaf_Stem:} {Ratio between Leaves Biomass and Stem Biomass}
+#'   \item{NBsh:} {Number of Primary Shoots per Plant}
+#'   \item{NBI:} {Number of Phytomers per Primary Axes of Plant}
+#'   \item{NBD1:} {Number of Dormant Buds (type I)}
+#'   \item{NBB:} {Number of Buds (Type B)}
+#'   \item{FTSW:} {Fraction of Transpirable Soil Water}
+#'   \item{Etransp:} {Daily Plant Transpiration, in \ifelse{html}{\out{m<sup>3</sup>}}{\eqn{m^3}}}
+#'   \item{DemandC_Feuil:} {Leaves Demand for Carbon}
+#'   \item{DemandN_Pet:} {Petioles Demand for Nitrogen}
+#'   \item{DemandN_Stem:} {Stems Demand for Nitrogen}
+#'   \item{DemandN_Tot:} {Plant Demand for Nitrogen}
+#'   \item{DemandN_Tot_Aer:} {Shoots Demands for Nitrogen}
+#'   \item{Npc_aer:} {Actual Nitrogen Concentration of the Canopy, in %}
+#'   \item{NNI:} {Plant Nitrogen Nutrition Index}
+#'   \item{Ndfa:} {Proportion of N derived from Fixation}
+#'   \item{Qfix:} {Plant Atmospheric N Fixation, in g}
+#'   \item{Naerien:} {Total Plant Shoot N content, in g}
+#'   \item{Nuptake_sol:} {Plant Mineral N Uptake, in g}
+#'   \item{R_DemandC_Root:} {Carbon Offer to Demand Ratio for Roots}
+#'   \item{SRL:} {Specific Root Length, in m}
+#'   \item{dMSenFeuil} {}
+#'   \item{dMSenTige} {}
+#'   \item{MS_pivot} {Taproots Dry Biomass, in g}
+#'   \item{MS_rac_fine} {Fine Roots Dry Biomass, in g}
+#'   \item{R_DemandC_Shoot} {}
+#'   \item{RUEpot} {Potential Radiation Use Efficiency, in \ifelse{html}{\out{g.J.PAR<sup>-1</sup>}}{\eqn{g.J.PAR^-1}} }
+#'   \item{RUE} {Radiation Use Efficiency, in \ifelse{html}{\out{g.J.PAR<sup>-1</sup>}}{\eqn{g.J.PAR^-1}}}
+#'   \item{Npc_piv:} {Actual Nitrogen Concentration of Taproots , in %}
+#'   \item{Npc_rac_fine:} {Actual Nitrogen Concentration of Fine Roots, in %}
+#'   \item{dRLenSentot} {Variation of Root Length, in m}
+#'   \item{dMSenRoot} {}
+#'   \item{RLTotNet} {Length of roots actively growing, in m}
+#'   \item{MS_rac_fineNet} {Biomass of Fine Roots, in g}
+#'   \item{perteN_rac_fine} {N losses due to the senescence of fine roots, in g}
+#'   \item{NBphyto:} {Total Number of Phytomers}
+#'   \item{NBapexAct:} {Number of Active Apex}
+#'   \item{transpi:} {Daily Plant Transpiration, in \ifelse{html}{\out{m<sup>3</sup>}}{\eqn{m^3}}}
+#'   \item{cumtranspi:} {Cumulative Transpiration from the start of Plant Emergence, in \ifelse{html}{\out{m<sup>3</sup>}}{\eqn{m^3}}}
+#'   \item{aliveB} {}
+#'   \item{TTphyllo:} {Phyllocronic Time, in phyllochron}
+#'   \item{dTT:} {Daily Variation of Thermal Time}
+#'   \item{Udev:} {Unit of Development}
+#'   \item{Udevstress} {}
 #' }
 #'
 #' @docType data
 #' @keywords datasets
 #' @name toto
 #' @usage data(toto)
-#' @format A data frame with 53940 rows and 10 variables
+#' @format A data frame with 16776 rows and 66 variables
 NULL
