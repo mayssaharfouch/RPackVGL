384bf66ab615cc6d6efb63765c15a2e6867410ba
mayssaharfouch <mayssaharfouch1@gmail.com>
2022-04-27 22:09:09 +0200

    Adding a new dataset

---
+++ R/management.R
@@ -0,0 +1,25 @@
+#' Model Inputs
+#'
+#' Mnagement input example
+#'
+#' \itemize{
+#'   \item{Date:} {Date mm/dd/yy}
+#'   \item{year:} {year}
+#'   \item{month:} {month}
+#'   \item{day:} {day}
+#'   \item{DOY:} {Day of the Year}
+#'   \item{Coupe:} {Days of the cuts}
+#'   \item{Irrig:} {Irrigation}
+#'   \item{FertNO3:} {NO3 Fertilization, in kg/ha}
+#'   \item{FertNH4:} {NH4 Fertilization, in kg/ha}
+#'   \item{Hcut:} {Height of the cut, in cm}
+
+
+#' }
+#'
+#' @docType data
+#' @keywords input dataset
+#' @name management
+#' @usage data(management)
+#' @format A data frame with 367 rows and 10 variables
+NULL
